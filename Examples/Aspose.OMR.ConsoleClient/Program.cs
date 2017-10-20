55de4276100c1eec7597425cd61e48d6e6b125df
Ikram ul Haq <mikram_ul_haq@hotmail.com>
2017-10-20 13:05:53 +0500

    Aspose.OMR for Cloud 17.10 - Changes

--- Examples/Aspose.OMR.ConsoleClient/Program.cs
+++ Examples/Aspose.OMR.ConsoleClient/Program.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2017 Aspose Pty Ltd. All Rights Reserved.
+ *
+ * Licensed under the MIT (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      https://github.com/aspose-omr/Aspose.OMR-for-Cloud/blob/master/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -10,61 +26,54 @@ namespace Aspose.OMR.ConsoleClient
 {
     class Program
     {
-        // provide your own keys recieved by registrating at Aspose Cloud Dashboard
-        private static string APIKEY = "xxxxxxx";
-        private static string APPSID = "xxxxxxx";
-        private static string BASEPATH = "http://api.aspose.cloud/v1.1";
+        // provide your own keys recieved by registrating at Aspose Cloud Dashboard (https://dashboard.aspose.cloud/)
+        private static string APIKEY = "xxxxx";
+        private static string APPSID = "xxxxx";
 
-
+        /// <summary>
+        /// Production base path
+        /// </summary>
+        private static string BASEPATH = "https://api.aspose.cloud/v1.1";
+        
+        /// <summary>
+        /// Path to test data
+        /// </summary>
         static string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\");
 
         static void Main(string[] args)
         {
-            // 1- First step is to Create an OMR template using Aspose.OMR.Client application 
-            // 2- Once your are done with OMR template creation, you can perform actions:
-            // Correct Template
-            // Finalize Template
-            // Recognize Image
-            // Above actions can be performed using REST API calls
-
-            // Action #1: Run template correction
+            // 0. Create template (using Aspose.OMR.Client)
+            // 1. Run template correction
             OMRResponse response = RunOmrTask("CorrectTemplate", "original.jpg", File.ReadAllText(path + "100qSimple.omr"));
 
-            // Get template id from Json response, which will be used during finalization and recognition process
+            // get template id, which will be used during finalization and recognition
             string templateId = response.Payload.Result.TemplateId;
 
-            // Save copy of corrected template file on you disk
+            // save corrected template data to file
             File.WriteAllBytes(path + "CorrectedTemplate.bin", response.Payload.Result.ResponseFiles[0].Data);
 
-            // Action #2: Run template Finalization
+            // 2. Run template Finalization
             RunOmrTask("FinalizeTemplate", "CorrectedTemplate.bin", templateId);
 
-            // Action #3: Run Recognize image
+            // 3. Recognize image
             OMRResponse recognitionResponse = RunOmrTask("RecognizeImage", "photo.jpg", templateId);
 
-            // Get recognition results as string from response Json
+            // get recognition results as string
             string recognitionResults = Encoding.UTF8.GetString(recognitionResponse.Payload.Result.ResponseFiles[0].Data);
 
             Console.WriteLine(recognitionResults);
             Console.ReadLine();
         }
 
-        /// <summary>
-        /// Function to perform different actions on the template
-        /// </summary>
-        /// <param name="actionName">Name of action that is needed to performed</param>
-        /// <param name="inputFileName">Input file name e.g. an image</param>
-        /// <param name="functionParam">It can be some ID of an OMR template or OMR template</param>
-        /// <returns></returns>
         private static Com.Aspose.OMR.Model.OMRResponse RunOmrTask(string actionName, string inputFileName, string functionParam)
         {
-            // Create an instance of Aspose Storage Cloud API SDK
+            // Instantiate Aspose Storage Cloud API SDK
             OmrApi target = new OmrApi(APIKEY, APPSID, BASEPATH);
 
-            // Create an instance of Aspose OMR Cloud API SDK
+            // Instantiate Aspose OMR Cloud API SDK
             StorageApi storageApi = new StorageApi(APIKEY, APPSID, BASEPATH);
 
-            // Create an instance of function parameters
+            // Init function parameters
             OMRFunctionParam param = new OMRFunctionParam();
             param.FunctionParam = functionParam;
 
@@ -72,12 +81,11 @@ namespace Aspose.OMR.ConsoleClient
             string storage = null;
             string folder = null;
 
-            // Upload source file to Aspose cloud storage
+            // Upload source file to aspose cloud storage
             storageApi.PutCreate(inputFileName, "", "", System.IO.File.ReadAllBytes(path + inputFileName));
 
-            // Invoke Aspose.OMR Cloud SDK REST API
+            // Invoke Aspose.OMR Cloud SDK API
             Com.Aspose.OMR.Model.OMRResponse response = target.PostRunOmrTask(inputFileName, actionName, param, storage, folder);
-
             return response;
         }
     }
